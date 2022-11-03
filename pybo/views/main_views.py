2acba6cbbf1bbdbc6417aa3c1bf2045070b2b741
MaJaeKim <mandori21@naver.com>
2022-11-03 13:17:59 +0900

    logs 출력

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -4,8 +4,7 @@ from werkzeug.utils import redirect
 bp = Blueprint( 'main',__name__, url_prefix='/' )
 
 @bp.route('/')
-def index():
-    3/0
+def index():    
     return redirect( url_for('question._list'))
 
 
