749dcd4b9118df1fd52cd35d574ed141da6de657
MaJaeKim <mandori21@naver.com>
2022-11-03 13:13:41 +0900

    logs 출력

--- config/production.py
+++ config/production.py
@@ -6,25 +6,27 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SECRET_KEY = b'\x86\x82\xe8\x87Di\x1d\x96\xe4,j3h\xc6c\x0b'
 
+
+
 dictConfig({
-    'version':1,
-    'formatters':{
+    'version': 1,
+    'formatters': {
         'default': {
-            'format':'[%(asctime)s] %(levelname)s in %(module)s: %(message)s',                
-            }
-        },
-    'handlers':{
-        'file':{
-            'level':'INFO',
-            'class': 'logging.handler.RotatingFileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs/flasktest.log'),
-            'maxBytes': 1024*1024*5,
-            'backupCount':5,
-            'formatter':'default',            
-            },
-        },
-    'root':{
-        'level':'INFO',
-        'handler':['file']
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
         }
-})        
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
